c20c36b082e30803cbb487f149af444f964fdc25
Reginaldo Soares <reginaldolsoares@gmail.com>
2017-12-07 15:12:12 +0100

    Binary Tree Operations

---
+++ src/main/java/Node.java
@@ -0,0 +1,15 @@
+/* Java program to determine if binary tree is
+   height balanced or not */
+
+/* A binary tree node has data, pointer to left child,
+   and a pointer to right child */
+
+public class Node {
+    int data;
+    Node left, right;
+
+    Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
